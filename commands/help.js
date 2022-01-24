0e216ed7a88d5e210c255414206d5e1777ca47e6
Siddhant <siddhantsharma669@gmail.com>
2022-01-24 15:21:03 +0530

    Modularity help

---
+++ commands/help.js
@@ -0,0 +1,10 @@
+function helpfn(){
+    console.log(`List of all the commands
+               1) Tree command - node FO.js tree <dirname>
+               2) Organize Command - node FO.js oraganize <dirname>
+               3) Help Command - node FO.js help `)//Her in this space is considered as well
+}
+
+module.exports = {//never call the function in an object instead just pass its name and dont call it
+    helpkey : helpfn
+}
